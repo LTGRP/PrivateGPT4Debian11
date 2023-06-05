19a5c44fb1e6ddc0a2e5ded89e6ed982e78ae116
LTGRP Heavy Industries <66345220+LTGRP@users.noreply.github.com>
2023-06-05 10:11:48 -0400

    Updated installer.sh to conform with Debian 11
    Work in progress

--- installer.sh
+++ installer.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
-
+#!/bin/bash
+echo "This installer works on Debian 11"
 # License: GNU GPLv3
+cd ~
+git clone https://github.com/imartinez/privateGPT && cd /home/$USER/privateGPT && pip3.10 install -r requirements.txt'
+mkdir /home/models
+cd /home/models
+## More models located here ==> https://gpt4all.io/index.html
+wget -b --quiet https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin
+echo "Downloading ggml-gpt4all-j-v1.3-groovy.bin to /home/models dir"
+GPT4ALL="/home/models/ggml-gpt4all-j-v1.3-groovy.bin"
+cd ~
+wget --quiet https://raw.githubusercontent.com/LTGRP/PrivateGPT4Debian11/main/.env
+ENV="~/.env"
 
-git clone https://github.com/imartinez/privateGPT && cd /home/$USER/privateGPT && pip install -r requirements.txt && su -c '
-GPT4ALL="curl -LO https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin"
-ENV="curl -LO https://raw.githubusercontent.com/MichaelSebero/PrivateGPT4Linux/main/.env"
-
-mkdir /home/models && cd /home/models && eval "$GPT4ALL" && cd /home/$USER/privateGPT && eval "$ENV" && chmod 777 /home/$USER/privateGPT/.env && chmod 777 /home/models -R'
+mkdir /home/models && cd /home/models && eval "$GPT4ALL" && cd /home/$USER/privateGPT && eval "$ENV" && chmod 755 /home/$USER/privateGPT/.env && chmod 775 /home/models -R'
